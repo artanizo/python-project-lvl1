6374e5741f99e8ba78fa980668e8a5eb27dcda56
Irina Fomina <perk.jerk@gmail.com>
2022-04-04 17:22:42 +0300

    fix linting

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,13 +5,13 @@ description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(num):
-    for n in range(2, int(num**0.5)+1):
+    for n in range(2, int(num ** 0.5) + 1):
         if num % n == 0:
             return False
     return True
 
 
 def get_puzzle():
-  num = randint(2, 50)
-  answer = 'yes' if is_prime(num) else 'no'
-  return (num, answer)
+    num = randint(2, 50)
+    answer = 'yes' if is_prime(num) else 'no'
+    return (num, answer)
