8ea1d0485def12485a5aa886365bed6b27bf40ef
Irina Fomina <perk.jerk@gmail.com>
2022-04-04 14:59:59 +0300

    add gcd game

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,12 @@
+from math import gcd
+from random import randint
+
+description = 'Find the greatest common divisor of given numbers.'
+
+
+def get_puzzle():
+    first_number = randint(0, 100)
+    second_number = randint(0, 100)
+    answer = gcd(first_number, second_number)
+    question = f'{first_number} {second_number}'
+    return (question, str(answer))
